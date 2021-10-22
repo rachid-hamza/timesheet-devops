cc7e4853c8079eff22d6dad191dce8a4a1fe556b
RachidHAMZA <rachidtahar.hamza1@esprit.tn>
2021-10-22 03:15:16 +0200

    add comm test

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -28,7 +28,7 @@ public class User implements Serializable {
 	private String firstName; 
 	
 	private String lastName;
-	
+	//test git
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	
